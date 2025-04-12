ffa9286403ec7a16eee5454ee01cddb9150951ce
Samaa <samaaayman33@gmail.com>
2025-04-12 06:44:31 -0700

    colors file and home screen

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color primary = Colors.blueAccent;
+
+  static final Color primaryLight = Colors.indigo[200]!;
+
+  static const Color background = Colors.white;
+
+  static const Color textPrimary = Colors.black87;
+
+  static const Color textSecondary = Colors.grey;
+
+  static final Color fieldBorder = Colors.grey[300]!;
+
+  static const Color error = Colors.red;
+
+  static const Color success = Colors.green;
+
+  static final Color iconLight = Colors.grey[400]!;
+
+  static final Color primaryOpacity50 = Colors.blueAccent.withOpacity(0.5);
+  static final Color primaryOpacity30 = Colors.blueAccent.withOpacity(0.3);
+}
